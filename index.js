c6df80bd98cf6274071d68981962db36af6eddaa
Dawid Płocki <dawid.plocki@gmail.com>
2019-09-12 13:06:18 +0200

    Implementing greatings protocol

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello world");
+const assert = require('assert');
+
+class Bot
+{
+    message(message) {
+        message = message.toLowerCase();
+        if (message !== 'hi') {
+            return null;
+        }
+
+        return "Hello";
+    }
+}
+
+
+let bot;
+
+// Waiting for "hi"
+bot = new Bot();
+assert.strictEqual(bot.message("hi"), "Hello", "Bot should response with 'Hello'");
+assert.strictEqual(bot.message("Hi"), "Hello", "Bot should response with 'Hello'");
+
+bot = new Bot();
+assert.strictEqual(bot.message("no-Hi"), null, "Bot should ignore everything except 'hi'");
