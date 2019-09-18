f5dead3e67395244e92191a2e8aaa69773bab520
Dawid Płocki <dawid.plocki@gmail.com>
2019-09-18 11:46:58 +0200

    Parser now creates the states map

--- test/parser.js
+++ test/parser.js
@@ -3,39 +3,72 @@ const WaitForActivationState = require('../states/WaitForActivationState');
 const ChooseState = require('../states/ChooseState');
 
 
-function parser(description) {
-    const stateFileName = description.type.split('_')
-        .map(x => x.charAt(0).toUpperCase() + x.slice(1))
-        .join('') + 'State';
+function parser(statesArray) {
+    return statesArray.reduce((previous, current) => {
+            const stateFileName = current.type.split('_')
+                .map(x => x.charAt(0).toUpperCase() + x.slice(1))
+                .join('') + 'State';
 
-    const state = require(`../states/${stateFileName}`)
+            const state = require(`../states/${stateFileName}`)
 
-    return new state(description);
+            previous[current.name] = new state(current);
+
+            return previous;
+        }, {});
 }
 
 
 it('should return correct waitForActivation', function() {
     const activation = 'hi';
-    const result = parser({
-        name: 'Test',
+    const name = "Test";
+    const result = parser([{
+        name: name,
         type: 'wait_for_activation',
         activation: activation,
         actions: []
-    });
+    }]);
 
-    Assert.ok(result instanceof WaitForActivationState, "Parser should return correct class");
-    Assert.strictEqual(result.activation, activation, "Parser should return class with correct paramaters");
+    Assert.strictEqual(Object.keys(result).length, 1, "Parser should return map with one element array");
+    Assert.ok(result[name] instanceof WaitForActivationState, "Parser should return correct class");
+    Assert.strictEqual(result[name].activation, activation, "Parser should return class with correct paramaters");
 });
 
 it('should return correct ChooseState', function() {
     const initText = 'Choose your pain';
-    const result = parser({
-        name: 'Test',
+    const name = "Test";
+    const result = parser([{
+        name: name,
         type: 'choose',
         initText: initText,
         replays: []
-    });
+    }]);
 
-    Assert.ok(result instanceof ChooseState, "Parser should return correct class");
-    Assert.strictEqual(result.initText, initText, "Parser should return class with correct paramaters");
+    Assert.strictEqual(Object.keys(result).length, 1, "Parser should return map with one element array");
+    Assert.ok(result[name] instanceof ChooseState, "Parser should return correct class");
+    Assert.strictEqual(result[name].initText, initText, "Parser should return class with correct paramaters");
+});
+
+it('should return map of name to state', function() {
+    const firstStateName = 'first';
+    const secondStateName = 'second';
+    const paramForFirstState = {
+        name: firstStateName,
+        type: 'wait_for_activation',
+        activation: 'hi',
+        actions: []
+    };
+    const paramForSecondState = {
+        name: secondStateName,
+        type: 'choose',
+        initText: '',
+        replays: []
+    };
+    const expected = {
+        [firstStateName]: new WaitForActivationState(paramForFirstState),
+        [secondStateName]: new ChooseState(paramForSecondState)
+    };
+
+    const result = parser([paramForFirstState, paramForSecondState]);
+
+    Assert.deepStrictEqual(result, expected, "Parser should map the input into states map");
 });
