4de9dd2d4be67cd96439382fb6a8949d8b19c7ff
Dawid Płocki <dawid.plocki@gmail.com>
2019-09-18 17:46:57 +0200

    Adding simple bot client

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Nothing to show');
+const Bot = require('./Bot');
+const fs = require('fs');
+const parser = require('./parser');
+
+const contents = JSON.parse(fs.readFileSync('conversation.json', 'utf8'));
+const bot = new Bot(parser(contents));
+
+console.log('Can use the bot.message("") method to test the loaded conversation.');
+debugger;
