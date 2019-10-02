8cc62978f6928640e49ff64b379ff2b978357e01
Dawid Płocki <dawid.plocki@gmail.com>
2019-10-02 14:57:41 +0200

    Implementing action rememberInputAs

--- actions.js
+++ actions.js
@@ -2,30 +2,40 @@ function response(message) {
     return bot => bot.response = message;
 }
 
+
+function rememberInteralMechanism(message, key, value) {
+    if (!message.memory) {
+        message.memory = {};
+    }
+
+    message.memory[key] = value;
+}
+
+
 module.exports.response = response;
 
 module.exports.staySilence = function() {
     return response(null);
-}
+};
 
 module.exports.setState = function(state) {
     return bot => bot.state = state;
-}
+};
 
 module.exports.endConversation = function() {
     return bot => bot.reset();
-}
+};
 
 module.exports.jumpToState = function(stateName) {
     return bot => bot.jumpToState(stateName);
-}
+};
 
 module.exports.remember = function(key, value) {
-    return (_, message) => {
-        if (!message.memory) {
-            message.memory = {};
-        }
+    return (_, message) => rememberInteralMechanism(message, key, value);
+};
 
-        message.memory[key] = value;
-    }  
-}
+module.exports.rememberInputAs = function(key, transformFunction = null) {
+    const transform = transformFunction ? transformFunction : (m => m.text);
+    
+    return (_, message) => rememberInteralMechanism(message, key, transform(message));
+};
--- test/Bot.memory.js
+++ test/Bot.memory.js
@@ -1,23 +1,47 @@
 const Assert = require('assert');
 const MoveNextState = require('../states/MoveNextState');
 const { INIT_STATE } = require('../Bot.Consts');
-const { jumpToState, endConversation, remember } = require('../actions');
+const { jumpToState, endConversation, remember, rememberInputAs } = require('../actions');
 const Bot = require('../Bot');
 
 describe('Bot memory', function() {
+
+    function assertMemoryResponse(response, key, expectedValue) {
+        Assert.ok(response instanceof Object, "Incorrect response type");
+        Assert.ok(response.memory instanceof Object, "Missing memory in the response");
+        Assert.ok(response.memory.hasOwnProperty(key), "Memory has no requested key");
+        Assert.strictEqual(response.memory[key], expectedValue, "Memorised value is incorrect");
+    }
+
     it('should be accessible in the message', function() {
         const testKey = 'test_key';
         const testValue = 'test_value';
         const bot = new Bot({
-            [INIT_STATE]: new MoveNextState('hi', [remember('test_key', 'test_value'), jumpToState('first')]),
+            [INIT_STATE]: new MoveNextState('hi', [
+                remember(testKey, testValue),
+                jumpToState('first')
+            ]),
             'first': new MoveNextState('hi', [endConversation()])
         });
 
         const response = bot.message({ text: '1' });
 
-        Assert.ok(response instanceof Object, "Incorrect response type");
-        Assert.ok(response.memory instanceof Object, "Missing memory in the response");
-        Assert.ok(response.memory.hasOwnProperty(testKey), "Memory has no requested key");
-        Assert.strictEqual(response.memory[testKey], testValue, "Memorised value is incorrect");
+        assertMemoryResponse(response, testKey, testValue);
+    });
+
+    it('should remember the input', function() {
+        const testKey = 'test_key';
+        const testValue = 'test_value';
+        const bot = new Bot({
+            [INIT_STATE]: new MoveNextState('hi', [
+                rememberInputAs(testKey),
+                jumpToState('first')
+            ]),
+            'first': new MoveNextState('hi', [endConversation()])
+        });
+
+        const response = bot.message({ text: testValue });
+
+        assertMemoryResponse(response, testKey, testValue);
     });
 });
