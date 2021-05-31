9192d0b7f760debb59b50913d97bffd18661c1d3
Dawid Płocki <dawid.plocki@gmail.com>
2021-05-31 20:49:21 +0200

    Remove actions from index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Simplifier = require('./simplifier');
 const Bot = require('./bot');
 
 module.exports = {
-    actions: require('./actions'),
     Bot: require('./bot'),
     buildBot: (stateMap) => new Simplifier(new Bot(stateMap)),
     ParserBuilder: require('./parserBuilder'),
