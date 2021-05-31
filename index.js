92d72ba581c33f882e04180b7739ffddebe027d3
Dawid Płocki <dawid.plocki@gmail.com>
2021-05-31 20:46:24 +0200

    Fix syntax

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = {
     actions: require('./actions'),
     Bot: require('./bot'),
-    buildBot = require('./botBuilder'),
-    ParserBuilder = require('./parserBuilder'),
-    Simplifier = require('./simplifier'),
-    states = require('./states')
+    buildBot: require('./botBuilder'),
+    ParserBuilder: require('./parserBuilder'),
+    Simplifier: require('./simplifier'),
+    states: require('./states')
 }
